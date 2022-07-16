91342bc27aa4b3f92d2b69bde3a848129a4a6901
oliveiradiney <dineyoliveira1999@gmail.com>
2022-07-15 22:02:27 -0300

    StatusBar

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import AppLoading from 'expo-app-loading';
+
+import { StatusBar } from 'expo-status-bar';
+
 import {useFonts, DMSans_400Regular} from '@expo-google-fonts/dm-sans';
 import {DMSerifDisplay_400Regular} from '@expo-google-fonts/dm-serif-display';
 import { ThemeProvider } from 'styled-components/native';
 
-import theme from '@src/theme'
+import theme from './src/theme'
 
 import { SignIn } from '@screens/SignIn';
 
@@ -23,6 +26,11 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar
+        style='light'
+        translucent
+        backgroundColor='transparent'
+      />
       <SignIn />
     </ThemeProvider>
   );
